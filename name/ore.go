6e91cf5e8cb9bfa65a8655390333846e56ce499f
IvanKorchmit <criperadmiral92@gmail.com>
2023-08-24 15:16:11 +0300

    Upgrade template

---
+++ name/ore.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/IvanKorchmit/akevitt"
+)
+
+type Ore struct {
+	BaseItem
+	Price int
+}
+
+func (ore *Ore) Use(engine *akevitt.Akevitt, session *TemplateSession, other akevitt.GameObject) error {
+	return ore.onUse(engine, session)
+}
+func (ore *Ore) GetDescription() string { return ore.Description + fmt.Sprintf(" (%d$)", ore.Price) }
